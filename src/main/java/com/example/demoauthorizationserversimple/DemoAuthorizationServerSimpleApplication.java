0d25532bd1e3fe8a3ce6d362b03fb73376833256
KETOAN03 <ngocanha85@gmail.com>
2023-06-28 18:23:15 +0700

    first-commit

---
+++ src/main/java/com/example/demoauthorizationserversimple/DemoAuthorizationServerSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoauthorizationserversimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthorizationServerSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthorizationServerSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoauthorizationserversimple/DemoAuthorizationServerSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoauthorizationserversimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthorizationServerSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
